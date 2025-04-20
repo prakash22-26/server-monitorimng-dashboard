ac093ac3f003af1aabcdafcf757347c9b24b03ab
prakash22-26 <prakashkumarshah156@gmail.com>
2025-04-20 21:10:31 +0530

    Update App.js

--- frontendfirst/src/App.js
+++ frontendfirst/src/App.js
@@ -20,7 +20,7 @@ export default function ServerMonitoringDashboard() {
 
   useEffect(() => {
     const socket = new WebSocket(
-      "ws://localhost:8000/ws/updates"
+      "ws://https://server-monitorimng-dashboard-1.onrender.com/ws/updates"
     );
 
     socket.onopen = () => {
